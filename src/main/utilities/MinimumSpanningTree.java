3ab2e2d6b6bdc5e073bd8f56abc76f30f6b1a876
Georgios Ouzounis <geoconstructor625@gmail.com>
2015-05-22 20:39:58 +0300

    Added tests for main.utilities.MinimumSpanningTree.

--- src/main/utilities/MinimumSpanningTree.java
+++ src/main/utilities/MinimumSpanningTree.java
@@ -1,76 +1,246 @@
-package utilities;
+package main.utilities;
 
-import java.util.Arrays;
+import java.lang.IndexOutOfBoundsException;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.Map;
 
 public class MinimumSpanningTree{
   public MinimumSpanningTree(int numberOfVertices){
-    numberOfVertices_ = numberOfVertices;
-    
-    connections_ = new boolean[numberOfVertices_][numberOfVertices_];
-    for(int i = 0;i < numberOfVertices_;i++){
-      for(int j = 0;j < numberOfVertices_;j++){
+
+    connections_ = new boolean[numberOfVertices][numberOfVertices];
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = 0;j < numberOfVertices;j++){
         connections_[i][j] = (i == j);
       }
     }
   }
-  
+
   public MinimumSpanningTree(boolean[][] connections){
     connections_ = connections;
-
-    numberOfVertices_ = connections.length;
   }
-  
+
   public static MinimumSpanningTree kruskal(double[] edgeWeights,
                                                           int numberOfVertices){
-    
+
     boolean[][] connections = new boolean[numberOfVertices][numberOfVertices];
     for(int i = 0;i < numberOfVertices;i++){
       for(int j = 0;j < numberOfVertices;j++){
         connections[i][j] = (i == j);
       }
     }
-    
-    Arrays.sort(edgeWeights);
 
-    for(int i = 0;i < edgeWeights.length;i++){
-      
-    }
-  }
-  
-  public int getNumberOfVertices(){
-    return numberOfVertices_;
-  }
-  
-  // TODO
-  // Throw an exception if either of the two vertices is out of bounds.
-  public boolean areConnected(int vertex1, int vertex2){
-    return connections_[vertex1][vertex2];
-  }
-  
-  // TODO
-  // Throw an exception if either of the two vertices is out of bounds.
-  public void connect(int vertex1, int vertex2){
-    connections_[vertex1][vertex2] = true;
+    int[] indeces = Utilities.sortArray(edgeWeights);
+    int verticesLeft = numberOfVertices;
 
-    if(vertex1 != vertex2){
-      connections_[vertex2][vertex1] = true;
+    // Add the first connection to the tree.
+    // Notice that the first connection adds 2 vertices to the tree.
+    MinimumSpanningTree.addConnection(connections, indeces[0]);
+    verticesLeft -= 2;
+
+    for(int i = 1;i < indeces.length;i++){
+      if(!MinimumSpanningTree.createsCircle(connections, indeces[i])){
+        MinimumSpanningTree.addConnection(connections, indeces[i]);
+        verticesLeft--;
+        if(verticesLeft == 0){
+          break;
+        }
+      }
     }
+
+    return (new MinimumSpanningTree(connections));
   }
 
-  // TODO
-  // Throw an exception if either of the two vertices is out of bounds.
-  public void disconnect(int vertex1, int vertex2){
-    connections_[vertex1][vertex2] = false;
-    
-    if(vertex1 != vertex2){
-      connections_[vertex2][vertex1] = false;
+  public int numberOfVertices(){
+    return connections_.length;
+  }
+
+  public boolean areConnected(int vertex1, int vertex2)
+                                               throws IndexOutOfBoundsException{
+    if(vertex1 < 0 || vertex1 >= connections_.length ||
+       vertex2 < 0 || vertex2 >= connections_.length){
+      throw new IndexOutOfBoundsException();
+    }
+    else{
+      return connections_[vertex1][vertex2];
     }
   }
-  
-  // TODO
-  // Change so that the class saves only the need matrix, not symetric or
-  // main diagonal.
+
+  public void connect(int vertex1, int vertex2)
+                                               throws IndexOutOfBoundsException{
+    MinimumSpanningTree.connect(connections_, vertex1, vertex2);
+  }
+
+  public static void connect(boolean[][] connections, int vertex1, int vertex2)
+                                              throws IndexOutOfBoundsException{
+    if(vertex1 < 0 || vertex1 >= connections.length ||
+        vertex2 < 0 || vertex2 >= connections.length){
+       throw new IndexOutOfBoundsException();
+     }
+     else{
+       connections[vertex1][vertex2] = true;
+
+       if(vertex1 != vertex2){
+         connections[vertex2][vertex1] = true;
+       }
+     }
+  }
+
+  public void disconnect(int vertex1, int vertex2)
+                                               throws IndexOutOfBoundsException{
+    MinimumSpanningTree.disconnect(connections_, vertex1, vertex2);
+  }
+
+  public static void disconnect(boolean[][] connections, int vertex1,
+                                                                    int vertex2)
+                                               throws IndexOutOfBoundsException{
+    if(vertex1 < 0 || vertex1 >= connections.length ||
+        vertex2 < 0 || vertex2 >= connections.length){
+       throw new IndexOutOfBoundsException();
+     }
+     else{
+       connections[vertex1][vertex2] = false;
+
+       if(vertex1 != vertex2){
+         connections[vertex2][vertex1] = false;
+       }
+     }
+  }
+
+  private static void addConnection(boolean[][] connections, int index){
+    int[] matrixIndices = MinimumSpanningTree.
+                          vectorIndexToUpperTriangularIndeces(
+                                                     connections.length, index);
+
+    MinimumSpanningTree.connect(connections, matrixIndices[0],
+                                                              matrixIndices[1]);
+  }
+
+  private static int[] vectorIndexToUpperTriangularIndeces(int numberOfRows,
+                                                                     int index){
+    int rowIndex = 0;
+    int columnIndex = 0;
+
+    while(index >= numberOfRows - 1 - rowIndex){
+      index -= numberOfRows - 1 - rowIndex;
+      rowIndex++;
+    }
+    columnIndex = rowIndex + 1 + index;
+
+    return (new int[] {rowIndex, columnIndex});
+  }
+
+  private static boolean createsCircle(boolean[][] connections, int index){
+    int[] matrixIndices = MinimumSpanningTree.
+                          vectorIndexToUpperTriangularIndeces(
+                                                     connections.length, index);
+    int rowIndex = matrixIndices[0];
+    int columnIndex = matrixIndices[1];
+
+    int depth = 0;
+    if(rowIndex > columnIndex){
+      return (MinimumSpanningTree.doIReach(rowIndex, columnIndex, connections, depth));
+    }
+    else{
+      return (MinimumSpanningTree.doIReach(columnIndex, rowIndex, connections, depth));
+    }
+  }
+
+  private static boolean doIReach(int destination, int beginning,
+                                            boolean[][] connections, int depth){
+    if(beginning == destination){
+      return true;
+    }
+
+    int length = connections.length;
+    depth++;
+    if(depth == (length * (length -1)) / 2){
+      return false;
+    }
+
+    for(int j = 0;j < length;j++){
+      if(beginning == j){
+        continue;
+      }
+      if(!connections[beginning][j]){
+        continue;
+      }
+
+      if(doIReach(destination, j, connections, depth)){
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public int[][] getAllPathsUnique(){
+    // Get all the paths, with duplicates.
+    int[][] paths = this.getAllPaths();
+
+    // Add all the paths in a hash table, to eliminate duplicates.
+    Hashtable<Integer, Integer> hashTable = new Hashtable<Integer, Integer>();
+    for(int path = 0;path < paths.length;path++){
+      hashTable.put(new Integer(this.pathHashKey(paths[path])), new Integer(path));
+    }
+
+    // Convert the hash table to an array of integer arrays.
+    int[][] newPaths = new int[hashTable.size()][];
+    int index = 0;
+    Iterator<Map.Entry<Integer, Integer> > iterator = hashTable.entrySet().iterator();
+    while(iterator.hasNext()){
+      Map.Entry<Integer, Integer> entry = iterator.next();
+
+      newPaths[index] = paths[entry.getValue().intValue()];
+      index++;
+    }
+
+    return newPaths;
+  }
+
+  private int pathHashKey(int[] path){
+    int key = 0;
+
+    for(int i = 0;i < path.length;i++){
+      key ^= (int)(Math.pow(2, path[i]));
+    }
+
+    return key;
+  }
+
+  public int[][] getAllPaths(){
+    ArrayList<ArrayList<Integer> > paths = new ArrayList<ArrayList<Integer> >();
+
+    ArrayList<Integer> newPath;
+    for(int i = 0;i < connections_.length;i++){
+      newPath = new ArrayList<Integer>();
+      newPath.add(i);
+      paths.add(newPath);
+    }
+
+    paths = findPaths(paths);
+
+    // Transform paths to array of int arrays.
+    int[][] allPaths = new int[paths.size()][];
+    for(int i = 0;i < paths.size();i++){
+      allPaths[i] = new int[paths.get(i).size()];
+
+      for(int j = 0;j < allPaths[i].length;j++){
+        allPaths[i][j] = paths.get(i).get(j).intValue();
+      }
+    }
+
+    return allPaths;
+  }
+
+  private ArrayList<ArrayList<Integer> > findPaths(ArrayList<ArrayList<Integer> > existingPaths){
+    return (Utilities.findPaths(existingPaths, connections_));
+  }
+
+  public int[] getContext(int[] vertices){
+    return (Utilities.getContext(vertices, connections_));
+  }
+
   private boolean[][] connections_;
-  
-  private int numberOfVertices_;
 }
---
+++ src/tests/utilities/MinimumSpanningTreeTest.java
@@ -0,0 +1,280 @@
+package tests.utilities;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import main.utilities.MinimumSpanningTree;
+
+public class MinimumSpanningTreeTest{
+
+  /** \brief also tests areConnected and numberOfVertices.
+   *
+   */
+  @Test
+  public void testMinimumSpanningTree(){
+    int numberOfVertices = 5;
+    MinimumSpanningTree minimumSpanningTree =
+                                      new MinimumSpanningTree(numberOfVertices);
+    assertEquals(numberOfVertices, minimumSpanningTree.numberOfVertices(), 0);
+
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = 0;j < numberOfVertices;j++){
+        if(i == j){
+          assertTrue(minimumSpanningTree.areConnected(i, j));
+        }
+        else{
+          assertFalse(minimumSpanningTree.areConnected(i, j));
+        }
+      }
+    }
+
+    boolean[][] connections = new boolean[numberOfVertices][numberOfVertices];
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = 0;j < numberOfVertices;j++){
+        connections[i][j] = !(i == j);
+      }
+    }
+
+    minimumSpanningTree = new MinimumSpanningTree(connections);
+
+    assertEquals(numberOfVertices, minimumSpanningTree.numberOfVertices(), 0);
+
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = 0;j < numberOfVertices;j++){
+        if(i == j){
+          assertFalse(minimumSpanningTree.areConnected(i, j));
+        }
+        else{
+          assertTrue(minimumSpanningTree.areConnected(i, j));
+        }
+      }
+    }
+
+  }
+
+  @Test
+  public void testKruskal(){
+    double[] edgeWeights = new double[] {12, 100, 16, 100, 100, 100, 13,
+                                         16, 100, 100, 100, 14, 100,
+                                         12, 100, 14, 100, 100,
+                                         13, 100, 100, 100,
+                                         14, 100, 15,
+                                         15, 100,
+                                         14};
+    int numberOfVertices = 8;
+
+    MinimumSpanningTree minimumSpanningTree =
+                     MinimumSpanningTree.kruskal(edgeWeights, numberOfVertices);
+
+    boolean[][] correctConnections =
+                                new boolean[numberOfVertices][numberOfVertices];
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = 0;j < numberOfVertices;j++){
+        correctConnections[i][j] = (i == j);
+      }
+    }
+    correctConnections[0][1] = true;
+    correctConnections[0][7] = true;
+    correctConnections[1][6] = true;
+    correctConnections[2][3] = true;
+    correctConnections[2][5] = true;
+    correctConnections[3][4] = true;
+    correctConnections[4][7] = true;
+
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = i + 1;j < numberOfVertices;j++){
+        if(correctConnections[i][j]){
+          assertTrue(minimumSpanningTree.areConnected(i, j));
+        }
+        else{
+          assertFalse(minimumSpanningTree.areConnected(i, j));
+        }
+      }
+    }
+
+  }
+
+  @Test
+  public void testConnectDisconnect(){
+    int numberOfVertices = 10;
+    MinimumSpanningTree minimumSpanningTree =
+                                      new MinimumSpanningTree(numberOfVertices);
+
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = 0;j < numberOfVertices;j++){
+        if(j == i){
+          minimumSpanningTree.connect(i, j);
+        }
+        else{
+          minimumSpanningTree.disconnect(i, j);
+        }
+      }
+    }
+
+    for(int i = 0;i < numberOfVertices;i++){
+      for(int j = 0;j < numberOfVertices;j++){
+        if(j == i){
+          assertTrue(minimumSpanningTree.areConnected(i, j));
+        }
+        else{
+          assertFalse(minimumSpanningTree.areConnected(i, j));
+        }
+      }
+    }
+
+  }
+
+  @Test
+  public void testAddConnection() throws IllegalArgumentException,
+                                         InvocationTargetException,
+                                         IllegalAccessException,
+                                         NoSuchMethodException,
+                                         SecurityException{
+    Class[] arguments = new Class[2];
+    arguments[0] = boolean[][].class;
+    arguments[1] = int.class;
+    Method addConnection = MinimumSpanningTree.class.getDeclaredMethod(
+                                                    "addConnection", arguments);
+    addConnection.setAccessible(true);
+
+    boolean[][] connections = new boolean[5][5];
+    for(int i = 0;i < 5;i++){
+      for(int j = 0;j < 5;j++){
+        connections[i][j] = false;
+      }
+    }
+
+    addConnection.invoke(null, connections, 5);
+    assertTrue(connections[1][3]);
+
+    addConnection.invoke(null, connections, 9);
+    assertTrue(connections[3][4]);
+  }
+
+  @Test
+  public void testVectorIndexToUpperTriangularIndeces()
+                                                throws IllegalArgumentException,
+                                                      InvocationTargetException,
+                                                      IllegalAccessException,
+                                                      NoSuchMethodException,
+                                                      SecurityException{
+    Class[] arguments = new Class[2];
+    arguments[0] = int.class;
+    arguments[1] = int.class;
+    Method vectorIndexToUpperTriangularIndeces = MinimumSpanningTree.class.
+       getDeclaredMethod("vectorIndexToUpperTriangularIndeces", arguments);
+
+    vectorIndexToUpperTriangularIndeces.setAccessible(true);
+
+    int[] indices;
+
+    indices = (int[])(vectorIndexToUpperTriangularIndeces.invoke(null, 5, 3));
+    assertEquals(0, indices[0], 0);
+    assertEquals(4, indices[1], 0);
+
+    indices = (int[])(vectorIndexToUpperTriangularIndeces.invoke(null, 4, 4));
+    assertEquals(1, indices[0], 0);
+    assertEquals(3, indices[1], 0);
+  }
+
+  @Test
+  public void testCreatesCircle() throws IllegalArgumentException,
+                                         InvocationTargetException,
+                                         IllegalAccessException,
+                                         NoSuchMethodException,
+                                         SecurityException{
+    Class[] arguments = new Class[2];
+    arguments[0] = boolean[][].class;
+    arguments[1] = int.class;
+    Method createsCircle = MinimumSpanningTree.class.getDeclaredMethod(
+                                                    "createsCircle", arguments);
+    createsCircle.setAccessible(true);
+
+    boolean[][] connections = new boolean[4][4];
+    for(int i = 0;i < connections.length;i++){
+      for(int j = 0;j < connections.length;j++){
+        connections[i][j] = (i == j);
+      }
+    }
+
+    connections[0][1] = true;
+    connections[1][3] = true;
+
+    assertTrue((boolean)(createsCircle.invoke(null, connections, 2)));
+    assertFalse((boolean)(createsCircle.invoke(null, connections, 5)));
+  }
+
+  @Test
+  public void testDoIReach() throws IllegalArgumentException,
+                                    InvocationTargetException,
+                                    IllegalAccessException,
+                                    NoSuchMethodException,
+                                    SecurityException{
+    Class[] arguments = new Class[4];
+    arguments[0] = int.class;
+    arguments[1] = int.class;
+    arguments[2] = boolean[][].class;
+    arguments[3] = int.class;
+    Method doIReach = MinimumSpanningTree.class.getDeclaredMethod("doIReach",
+                                                                     arguments);
+    doIReach.setAccessible(true);
+
+    boolean[][] connections = new boolean[4][4];
+    for(int i = 0;i < connections.length;i++){
+      for(int j = 0;j < connections.length;j++){
+        connections[i][j] = (i == j);
+      }
+    }
+
+    connections[0][1] = true;
+    connections[1][3] = true;
+
+    int depth = 0;
+    assertTrue((boolean)(doIReach.invoke(null, 3, 0, connections, depth)));
+    assertFalse((boolean)(doIReach.invoke(null, 3, 2, connections, depth)));
+  }
+
+  @Test
+  public void testGetAllPaths(){
+    double[] edgeWeights = new double[] {12, 100, 16, 100, 100, 100, 13,
+                                         16, 100, 100, 100, 14, 100,
+                                         12, 100, 14, 100, 100,
+                                         13, 100, 100, 100,
+                                         14, 100, 15,
+                                         15, 100,
+                                         14};
+    int numberOfVertices = 8;
+
+    MinimumSpanningTree minimumSpanningTree =
+                     MinimumSpanningTree.kruskal(edgeWeights, numberOfVertices);
+
+    int[][] paths = minimumSpanningTree.getAllPaths();
+
+    assertEquals(502, paths.length, 0);
+  }
+
+  @Test
+  public void testGetAllPathsUnique(){
+    double[] edgeWeights = new double[] {12, 100, 16, 100, 100, 100, 13,
+                                         16, 100, 100, 100, 14, 100,
+                                         12, 100, 14, 100, 100,
+                                         13, 100, 100, 100,
+                                         14, 100, 15,
+                                         15, 100,
+                                         14};
+    int numberOfVertices = 8;
+
+    MinimumSpanningTree minimumSpanningTree =
+        MinimumSpanningTree.kruskal(edgeWeights, numberOfVertices);
+
+    int[][] paths = minimumSpanningTree.getAllPathsUnique();
+
+    assertEquals(36, paths.length, 0);
+  }
+}
