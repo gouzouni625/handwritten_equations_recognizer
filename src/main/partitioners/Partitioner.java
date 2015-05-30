dd0fe3add96eb914db9a49c0952f1dc78db821c5
Georgios Ouzounis <geoconstructor625@gmail.com>
2015-05-30 19:29:34 +0300

    Removed get and set methods for the classifier in main.partitioners.Partitioner.

--- src/main/partitioners/Partitioner.java
+++ src/main/partitioners/Partitioner.java
@@ -5,14 +5,6 @@ import main.utilities.TraceGroup;
 
 public abstract class Partitioner{
   public abstract TraceGroup[] partition(TraceGroup expression);
-  
-  public void setClassifier(Classifier classifier){
-    classifier_ = classifier;
-  }
-  
-  public Classifier getClassifier(){
-    return classifier_;
-  }
-  
+
   protected Classifier classifier_;
 }
