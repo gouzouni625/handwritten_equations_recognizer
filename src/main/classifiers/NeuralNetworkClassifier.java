22c6241e6f07310f4611d7eca0ffbc02351068d5
Georgios Ouzounis <geoconstructor625@gmail.com>
2015-06-11 14:50:06 +0300

    Reformed main.classifiers.NeuralNetworkClassifier.

--- src/main/classifiers/NeuralNetworkClassifier.java
+++ src/main/classifiers/NeuralNetworkClassifier.java
@@ -2,12 +2,12 @@ package main.classifiers;
 
 import java.io.IOException;
 
+import main.base.NeuralNetwork;
 import main.utilities.TraceGroup;
+import main.utilities.Utilities;
 
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
-
-import base.NeuralNetwork;
 
 public class NeuralNetworkClassifier implements Classifier{
   public NeuralNetworkClassifier(int[] sizesOfLayers){
@@ -33,19 +33,86 @@ public class NeuralNetworkClassifier implements Classifier{
    *           valid symbol. The values lies inside [0, 1].
    */
   public double classify(TraceGroup symbol, TraceGroup context){
-    double[] output = neuralNetwork_.feedForward(this.imageToVector(symbol.toImage(new Size(28, 28)), -1, 1));
+    int symbolSize = symbol.size();
+    int contextSize = context.size();
 
-    double max = output[0];
-    int index = 0;
-    for(int i = 1;i < output.length;i++){
-      if(output[i] > max){
-        max = output[i];
-        index = i;
+    if(symbolSize > Utilities.MAX_TRACES_IN_SYMBOL){
+      return Utilities.MINIMUM_RATE;
+    }
+
+    double[] neuralNetworkOutput = neuralNetwork_.feedForward(this.imageToVector(symbol.print(new Size(28, 28)), -1, 1));
+    neuralNetworkOutput = Utilities.relativeValues(neuralNetworkOutput);
+
+    classificationLabel_ = Utilities.indexOfMax(neuralNetworkOutput);
+    double symbolRate = neuralNetworkOutput[classificationLabel_];
+
+    // Process context.
+    if(symbolSize == Utilities.MAX_TRACES_IN_SYMBOL || contextSize == 0){
+      return symbolRate;
+    }
+
+    boolean[][] connections = new boolean[contextSize][contextSize];
+    for(int i = 0;i < contextSize;i++){
+      for(int j = 0;j < contextSize;j++){
+        connections[i][j] = true;
       }
     }
-    classificationLabel_ = index;
+    int[][] contextPaths = Utilities.findUniquePaths(connections, Utilities.MAX_TRACES_IN_SYMBOL - symbolSize);
+    int numberOfContextPaths = contextPaths.length;
 
-    return max;
+    double finalRate = symbolRate;
+    TraceGroup symbolWithContext;
+    for(int i = 0;i < numberOfContextPaths;i++){
+      symbolWithContext = new TraceGroup(symbol);
+      symbolWithContext.add(context.subTraceGroup(contextPaths[i]));
+
+      neuralNetworkOutput = neuralNetwork_.feedForward(this.imageToVector(symbolWithContext.print(new Size(28, 28)), -1, 1));
+      neuralNetworkOutput = Utilities.relativeValues(neuralNetworkOutput);
+
+      double rate = Utilities.maxValue(neuralNetworkOutput);
+      if(rate < (Utilities.MAXIMUM_RATE - Utilities.MINIMUM_RATE) / 2){
+        continue;
+      }
+
+      finalRate -= 33 / 100 * rate;
+    }
+
+    /* ===== Print symbol rate ===== */
+    //System.out.println("symbolRate = " + symbolRate + " symbolLabel = " + classificationLabel_);
+    /* ===== Print symbol rate ===== */
+
+    /* ===== Print context paths ===== */
+    /*System.out.println(" ===== Print context paths =====");
+    for(int i = 0;i < contextPaths.length;i++){
+      for(int j = 0;j < contextPaths[i].length;j++){
+        System.out.print(contextPaths[i][j] + ", ");
+      }
+
+      System.out.println();
+    }
+    /* ===== Print context paths ===== */
+
+    /*TraceGroup symbolWithContext;
+    for(int i = 0;i < numberOfContextPaths;i++){
+      symbolWithContext = new TraceGroup(symbol);
+      symbolWithContext.add(context.subTraceGroup(contextPaths[i]));
+
+      neuralNetworkOutput = neuralNetwork_.feedForward(this.imageToVector(symbolWithContext.print(new Size(28, 28)), -1, 1));
+      neuralNetworkOutput = Utilities.relativeValues(neuralNetworkOutput);
+      int label = Utilities.indexOfMax(neuralNetworkOutput);
+      double rate = neuralNetworkOutput[label];
+
+      /* ===== Print symbol with context rate ===== */
+      //System.out.println("rate = " + rate + " label = " + label);
+      /* ===== Print symbol with context rate ===== */
+
+      /*if(Math.abs(rate - symbolRate) < (Utilities.MAXIMUM_RATE - Utilities.MINIMUM_RATE) / 2 && label != classificationLabel_){
+        classificationLabel_ = -1;
+        return Utilities.UNKNOWN_LABEL;
+      }
+    }*/
+
+    return finalRate;
   }
 
   /* TODO
