c31adf05cfe60773ec1e81c74c02f293ea74a4c6
Georgios Ouzounis <geoconstructor625@gmail.com>
2015-07-02 23:09:04 +0300

    Removed big and small classification from main.utilities.grmmarsGeometricalGrammar. Implemented opositePosition method.

--- src/main/utilities/grammars/GeometricalGrammar.java
+++ src/main/utilities/grammars/GeometricalGrammar.java
@@ -3,67 +3,23 @@ package main.utilities.grammars;
 public class GeometricalGrammar extends Grammar{
 
   public void parse(Symbol symbol1, Symbol symbol2){
+    // Find the relative position of the two symbols.
+    Symbol.ArgumentPosition relativePosition = this.relativePosition(symbol1, symbol2);
+
+    symbol1.setArgument(relativePosition, symbol2);
+    symbol2.setArgument(this.opositePosition(relativePosition), symbol1);
+  }
+
+  public Symbol.ArgumentPosition relativePosition(Symbol symbol1, Symbol symbol2){
     symbol1.traceGroup_.calculateCorners();
     symbol2.traceGroup_.calculateCorners();
 
-    double symbol1Area = symbol1.traceGroup_.getWidth() * symbol1.traceGroup_.getHeight();
-    double symbol2Area = symbol2.traceGroup_.getWidth() * symbol2.traceGroup_.getHeight();
-
-    /* ===== Logs ===== */
-    if(!silent_){
-      System.out.println("Log: symbols' areas... ===== Start =====");
-
-      System.out.println("Symbol 1 area: " + symbol1Area);
-      System.out.println("Symbol 2 area: " + symbol2Area);
-
-      System.out.println("Log: symbols' areas... ===== End =======");
-    }
-    /* ===== Logs ===== */
-
-    Symbol big;
-    Symbol small;
-
-    if(symbol1Area >= symbol2Area * 0.60){
-      big = symbol1;
-      small = symbol2;
-    }
-    else if(symbol2Area >= symbol1Area * 0.60){
-      big = symbol2;
-      small = symbol1;
-    }
-    else{
-      // There is no distinction between the big and the small symbol.
-      big = symbol1;
-      small = symbol2;
-    }
-
-    /* ===== Logs ===== */
-    if(!silent_){
-      System.out.println("Log: big and small symbols... ===== Start =====");
-
-      System.out.println("Big: " + big);
-      System.out.println("Small: " + small);
-
-      System.out.println("Log: big and small symbols... ===== End =======");
-    }
-    /* ===== Logs ===== */
-
-    // Find the relative position of the two symbols.
-    Symbol.ArgumentPosition relativePosition = this.relativePosition(big, small);
-
-    big.setArgument(relativePosition, small);
-  }
-
-  public Symbol.ArgumentPosition relativePosition(Symbol big, Symbol small){
-    big.traceGroup_.calculateCorners();
-    small.traceGroup_.calculateCorners();
-
     int yPosition;
-    if(small.traceGroup_.getBottomRightCorner().y_ >= big.traceGroup_.getBottomRightCorner().y_ + big.traceGroup_.getHeight() / 2){
+    if(symbol2.traceGroup_.getBottomRightCorner().y_ >= symbol1.traceGroup_.getBottomRightCorner().y_ + symbol1.traceGroup_.getHeight() / 2){
       yPosition = 1;
     }
-    else if(small.traceGroup_.getBottomRightCorner().y_ <= big.traceGroup_.getBottomRightCorner().y_ + big.traceGroup_.getHeight() / 2 &&
-            small.traceGroup_.getTopLeftCorner().y_ >= big.traceGroup_.getBottomRightCorner().y_ + big.traceGroup_.getHeight() / 2){
+    else if(symbol2.traceGroup_.getBottomRightCorner().y_ <= symbol1.traceGroup_.getBottomRightCorner().y_ + symbol1.traceGroup_.getHeight() / 2 &&
+            symbol2.traceGroup_.getTopLeftCorner().y_ >= symbol1.traceGroup_.getBottomRightCorner().y_ + symbol1.traceGroup_.getHeight() / 2){
       yPosition = 0;
     }
     else{
@@ -71,11 +27,11 @@ public class GeometricalGrammar extends Grammar{
     }
 
     int xPosition;
-    if(small.traceGroup_.getTopLeftCorner().x_ >= big.traceGroup_.getTopLeftCorner().x_ + big.traceGroup_.getWidth() / 2){
+    if(symbol2.traceGroup_.getTopLeftCorner().x_ >= symbol1.traceGroup_.getTopLeftCorner().x_ + symbol1.traceGroup_.getWidth() / 2){
       xPosition = 1;
     }
-    else if(small.traceGroup_.getTopLeftCorner().x_ <= big.traceGroup_.getTopLeftCorner().x_ + big.traceGroup_.getWidth() / 2 &&
-            small.traceGroup_.getBottomRightCorner().x_ >= big.traceGroup_.getTopLeftCorner().x_ + big.traceGroup_.getWidth() / 2){
+    else if(symbol2.traceGroup_.getTopLeftCorner().x_ <= symbol1.traceGroup_.getTopLeftCorner().x_ + symbol1.traceGroup_.getWidth() / 2 &&
+            symbol2.traceGroup_.getBottomRightCorner().x_ >= symbol1.traceGroup_.getTopLeftCorner().x_ + symbol1.traceGroup_.getWidth() / 2){
       xPosition = 0;
     }
     else{
@@ -131,4 +87,26 @@ public class GeometricalGrammar extends Grammar{
     }
   }
 
+  public Symbol.ArgumentPosition opositePosition(Symbol.ArgumentPosition position){
+    switch(position){
+      case ABOVE:
+        return Symbol.ArgumentPosition.BELOW;
+      case ABOVE_RIGHT:
+        return Symbol.ArgumentPosition.BELOW_LEFT;
+      case RIGHT:
+        return Symbol.ArgumentPosition.LEFT;
+      case BELOW_RIGHT:
+        return Symbol.ArgumentPosition.ABOVE_LEFT;
+      case BELOW:
+        return Symbol.ArgumentPosition.ABOVE;
+      case BELOW_LEFT:
+        return Symbol.ArgumentPosition.ABOVE_RIGHT;
+      case LEFT:
+        return Symbol.ArgumentPosition.RIGHT;
+      case ABOVE_LEFT:
+        return Symbol.ArgumentPosition.BELOW_RIGHT;
+      default:
+        return null;
+    }
+  }
 }
