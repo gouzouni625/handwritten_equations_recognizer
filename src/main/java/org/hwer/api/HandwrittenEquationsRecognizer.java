f65b7da7a869f662a25cbc03ce302c6d4e4b56b4
Georgios Ouzounis <geoconstructor625@gmail.com>
2016-04-18 23:32:48 +0300

    Add api functionality

--- src/main/java/org/hwer/api/HandwrittenEquationsRecognizer.java
+++ src/main/java/org/hwer/api/HandwrittenEquationsRecognizer.java
@@ -1,6 +1,10 @@
 package org.hwer.api;
 
 
+import org.hwer.api.concurrency.Consumer;
+import org.hwer.api.concurrency.tasks.EvaluateTask;
+import org.hwer.api.concurrency.tasks.RemoveTask;
+import org.hwer.api.concurrency.tasks.ResetParserTask;
 import org.hwer.engine.classifiers.NNClassifier;
 import org.hwer.engine.classifiers.NNClassifier.NeuralNetwork;
 import org.hwer.engine.classifiers.neural_network.NeuralNetworkImpl;
@@ -8,11 +12,14 @@ import org.hwer.engine.parsers.GrammarParser;
 import org.hwer.engine.parsers.grammars.GeometricalGrammar;
 import org.hwer.engine.partitioners.MSTPartitioner;
 import org.hwer.engine.symbols.SymbolFactory.Labels;
+import org.hwer.engine.utilities.traces.TraceGroup;
 import org.hwer.image_processing.CoreImpl;
 import org.hwer.image_processing.DistorterImpl;
 
 import java.io.IOException;
 import java.io.FileInputStream;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 
 
@@ -112,12 +119,107 @@ public class HandwrittenEquationsRecognizer {
         parser_ = new GrammarParser(new GeometricalGrammar());
 
         partitioner_.logger_.setLevel(Level.OFF);
+
+        tasksQueue_ = new ArrayBlockingQueue<>(5);
+        consumer_ = new Consumer(tasksQueue_);
+        new Thread(consumer_).start();
     }
 
     private String getResourcePath(String resourceName){
         return HandwrittenEquationsRecognizer.class.getResource(resourceName).getPath();
     }
 
+    public void terminate(){
+        consumer_.setRunning(false);
+    }
+
+    /**
+     * @brief Returns the String representation of the equation recognized
+     *
+     * @return The String representation of the equation recognized
+     */
+    public String getEquation () {
+        if (tasksQueue_.size() != 0) {
+            return "recognizing...";
+        }
+
+        String equation = parser_.getEquation();
+        if (equation.contains("AMBIGUOUS")) {
+            equation = "AMBIGUOUS";
+        }
+
+        return equation;
+    }
+
+    public void reset(){
+        tasksQueue_.clear();
+
+        resetParser();
+    }
+
+    /**
+     * @brief Schedules the Parser to be reset
+     *        The scheduling is done by adding a ResetParserTask to the tasksQueue.
+     *
+     * @return True if the tasksQueue has accepted the new task
+     */
+    private boolean resetParser () {
+        return tasksQueueOffer(new ResetParserTask(parser_));
+    }
+
+    /**
+     * @brief Schedules the given TraceGroup to be appended
+     *        The scheduling is done by adding an EvaluateTask to the tasksQueue.
+     *
+     * @param traceGroup
+     *     The TraceGroup to be appended
+     *
+     * @return True if the tasksQueue has accepted the new task
+     */
+    public boolean append (TraceGroup traceGroup) {
+        return traceGroup != null && traceGroup.size() != 0 &&
+            tasksQueueOffer(new EvaluateTask(partitioner_, parser_, traceGroup));
+    }
+
+    /**
+     * @brief Schedules the given TraceGroup to be removed
+     *        The scheduling is done by adding a RemoveTask to the tasksQueue.
+     *
+     * @param traceGroup
+     *     The TraceGroup to be removed
+     *
+     * @return True if the tasksQueue has accepted the new task
+     */
+    public boolean remove (TraceGroup traceGroup) {
+        return traceGroup != null && traceGroup.size() != 0 &&
+            tasksQueueOffer(new RemoveTask(partitioner_, parser_, traceGroup));
+    }
+
+    /**
+     * @brief Offers a tasks to the tasksQueue
+     *
+     * @param task
+     *     The task
+     *
+     * @return True if the tasksQueue accepts the task
+     */
+    private boolean tasksQueueOffer (Runnable task) {
+        try {
+            return tasksQueue_.offer(task, TASKS_QUEUE_OFFER_TIMEOUT, TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            return false;
+        }
+    }
+
     private MSTPartitioner partitioner_;
     private GrammarParser parser_;
+
+    private Consumer consumer_;
+
+    private ArrayBlockingQueue<Runnable> tasksQueue_;
+
+    private static final long TASKS_QUEUE_OFFER_TIMEOUT = 100; //!< The time to wait on the
+                                                               //!< tasksQueue to accept a task in
+                                                               //!< milliseconds
+
 }
---
+++ src/main/java/org/hwer/api/concurrency/tasks/ResetParserTask.java
@@ -0,0 +1,34 @@
+package org.hwer.api.concurrency.tasks;
+
+
+import org.hwer.engine.parsers.Parser;
+
+
+/**
+ * @class ResetParserTask
+ * @brief Task that resets a Parser
+ */
+public class ResetParserTask implements Runnable {
+    /**
+     * @brief Constructor
+     *
+     * @param parser
+     *     The Parser to be reset
+     */
+    public ResetParserTask (Parser parser) {
+        parser_ = parser;
+    }
+
+    /**
+     * @brief Runnable's run method
+     */
+    @Override
+    public void run () {
+        parser_.reset();
+
+        parser_.buildEquation();
+    }
+
+    private final Parser parser_; //!< The Parser of this task
+
+}
